1be45f8c2ede56fa7d64cf27ea756a556ee826e1
Gonzalo Suarez <gonzasuarezgagliardi@gmail.com>
2023-08-16 22:27:50 -0300

    Primera version v1

--- App.js
+++ App.js
@@ -4,7 +4,12 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hola señor tutor de la plataforma Coder</Text>
+      <Text>Amo los Pet Shop Boys :D</Text>
+      <Text>¿A vos te gustan?</Text>
+      <Text>En particular a mi me gustan mucho las siguientes canciones:</Text>
+      <Text>1. Love is a Bourgeois Construct - 2. Love Etc. - 3. So Hard</Text>
+      <Text>Escuchalas. Probablemente te gusten mucho.</Text>
       <StatusBar style="auto" />
     </View>
   );
